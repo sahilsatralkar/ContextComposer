9e068d5a338a28ed551f992afd67dc8ec020f8fe
Sahil Satralkar <62758655+sahilsatralkar@users.noreply.github.com>
2025-08-24 00:35:31 +0530

    Initial Commit

---
+++ ContextComposer/ContextComposerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextComposerApp.swift
+//  ContextComposer
+//
+//  Created by Sahil Satralkar on 24/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ContextComposerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
